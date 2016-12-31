42262fcc4b29eb5f0a040830b0059a7e9dcb9851
BuLLeTCode <raivis.rugelis@va.lv>
2016-12-31 20:33:01 +0900

    Added OneSignal push notification service. Added page for Journeys.

--- www/lib/global.js
+++ www/lib/global.js
@@ -1,6 +1,22 @@
 (function(){
-    //Push notification handler - Not really usefull - TODO: remove it.
-    monaca.cloud.Push.setHandler(function(data) {
-      alert(data);
-    });
+    //Global JS init
 })();
+
+    //OneSignal - push notification service
+    document.addEventListener('deviceready', function () {
+    // Enable to debug issues.
+    // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+    var notificationOpenedCallback = function(jsonData) {
+        console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    };
+    
+    window.plugins.OneSignal
+        .startInit("e0c9ffdc-adec-472b-b2e1-6b7821035f1a")
+        .handleNotificationOpened(notificationOpenedCallback)
+        .endInit();
+      
+      // Sync hashed email if you have a login system or collect it.
+      //   Will be used to reach the user at the most optimal time of day.
+      // window.plugins.OneSignal.syncHashedEmail(userEmail);
+    }, false);
